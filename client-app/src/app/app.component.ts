5a6e5b1390f7affea896b364ae540b759192257d
Samandar <uchqunovsamandar@mail.ru>
2022-07-15 11:44:47 -0700

    initial settings"

--- client-app/src/app/app.component.ts
+++ client-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  /**
+   *
+   */
+  constructor(private http:HttpClient) {
+    
+  }
+  ngOnInit(): void {
+    this.http.get("https://localhost:7154/api/Catigories").subscribe(res=>{
+      this.catigories = res as any[];
+    })
+  }
   title = 'client-app';
+  catigories:any[] = [];
+
 }
