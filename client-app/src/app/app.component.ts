16f93cc864788e414f38e0152d35b52af44f1f34
Samandar <uchqunovsamandar@mail.ru>
2022-07-22 11:22:26 -0700

    added initialize styles

--- client-app/src/app/app.component.ts
+++ client-app/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Book } from './Models/models';
 import { ApiService } from './Services/api.service';
 @Component({
   selector: 'app-root',
@@ -7,22 +6,12 @@ import { ApiService } from './Services/api.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  /**
-   *
-   */
+
   constructor(private apiSvc:ApiService) {
     
   }
   ngOnInit(): void {
-    this.apiSvc.getBooks().subscribe(res=>{
-      console.log(res)
-    })
-    this.apiSvc.getCatrigories().subscribe(res=>{
-      console.log(res)
-    })
-    this.apiSvc.getComments().subscribe(res=>{
-      console.log(res)
-    })
+   
   }
   title = 'client-app';
 
